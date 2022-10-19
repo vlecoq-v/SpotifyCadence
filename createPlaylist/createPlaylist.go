574257367921198e541c627ae6c2dc6ed9f5815e
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2022-10-19 18:12:42 +0200

    feat: increase size of playlist and artist seeds. bonus:code clarity

--- createPlaylist/createPlaylist.go
+++ createPlaylist/createPlaylist.go
@@ -9,11 +9,68 @@ import (
 )
 
 // create playlist matching target rythm and favorite artists
+// create an empty playlist and add recommandations from the seed
 func Create(client *spotify.Client) {
-	// wait for auth to complete
-	seeds := getSeeds(client)
+	ctx := context.Background()
+	const targetCadence = 180
+	const genericPlaylistDescription = "A playlist created by SpotifyCadence"
 
-	createPlaylist(client, seeds)
+	user, err := client.CurrentUser(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	seeds := getSeeds(client)
+	tracks := getTracks(client, seeds, user, targetCadence)
+
+	emptyPlaylist, playlistCreationErr := client.CreatePlaylistForUser(ctx, user.ID, fmt.Sprintf("spotifyCadence: %d ppm", targetCadence), genericPlaylistDescription, true, false)
+	if playlistCreationErr != nil {
+		log.Fatal("playlist creation error:", playlistCreationErr)
+	}
+
+	recoID := make([]spotify.ID, 0, len(tracks))
+	for _, v := range tracks {
+		recoID = append(recoID, v.ID)
+	}
+
+	for i := 0; i < len(tracks); i += 100 {
+		recoIdSubSlice := recoID[i:min(i+100, cap(recoID))]
+		_, addTracksErr := client.AddTracksToPlaylist(ctx, emptyPlaylist.ID, recoIdSubSlice...)
+		if addTracksErr != nil {
+			log.Fatal("error adding tracks to playlist: ", addTracksErr)
+		}
+	}
+
+	fmt.Println("Successfully created playlist ", emptyPlaylist.Name)
+}
+
+// get Tracks from seeds
+func getTracks(client *spotify.Client, seeds spotify.Seeds, user *spotify.PrivateUser, targetCadence float64) []spotify.SimpleTrack {
+	trackAttributes := spotify.NewTrackAttributes().
+		MinTempo(targetCadence/2 - 2).
+		MaxTempo(targetCadence/2 + 3).
+		MinEnergy(0.8)
+
+	var tracks []spotify.SimpleTrack
+
+	ctx := context.Background()
+	for i := 0; i < len(seeds.Artists); i += 5 {
+		seedSubSlice := spotify.Seeds{
+			Artists: seeds.Artists[i:min(i+5, cap(seeds.Artists))],
+		}
+		recommendations, recoError := client.GetRecommendations(ctx, seedSubSlice, trackAttributes, spotify.Country(user.Country), spotify.Limit(50))
+		if recoError != nil {
+			log.Fatal("reco error:", recoError)
+		}
+		tracks = append(tracks, recommendations.Tracks...)
+		fmt.Println("size reco :", len(recommendations.Tracks))
+	}
+
+	fmt.Println("total size reco :", len(tracks))
+	// jreco, _ := json.MarshalIndent(reco.Tracks, "", "\t")
+	// fmt.Printf(string(jreco))
+
+	return tracks
 }
 
 // get seeds from favorite artists
@@ -26,54 +83,19 @@ func getSeeds(client *spotify.Client) spotify.Seeds {
 	// fmt.Println(string(jTopArtists))
 
 	var seeds spotify.Seeds
-	for i, artist := range topArtists.Artists {
-		seeds.Artists = append(seeds.Tracks, artist.ID)
-		if i >= 5 {
-			break
-		}
+	var artistNames []string
+	for _, artist := range topArtists.Artists[:10] {
+		seeds.Artists = append(seeds.Artists, artist.ID)
+		artistNames = append(artistNames, artist.Name)
 	}
+	fmt.Println("size of seeds: ", len(seeds.Artists), "artist used", artistNames)
 
 	return seeds
 }
 
-// create an empty playlist and add recommandations from the seed
-func createPlaylist(client *spotify.Client, seeds spotify.Seeds) {
-	ctx := context.Background()
-
-	const targetCadence = 180
-	const genericPlaylistDescription = "A playlist created by SpotifyCadence"
-
-	user, err := client.CurrentUser(ctx)
-	if err != nil {
-		log.Fatal(err)
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
-
-	trackAttributes := spotify.NewTrackAttributes().
-		MinTempo(targetCadence/2 - 2).
-		MaxTempo(targetCadence/2 + 3)
-	reco, recoError := client.GetRecommendations(ctx, seeds, trackAttributes, spotify.Country(user.Country), spotify.Limit(50))
-	if recoError != nil {
-		log.Fatal("reco error:", recoError)
-	}
-
-	fmt.Println("size reco :", len(reco.Tracks))
-	// jreco, _ := json.MarshalIndent(reco.Tracks, "", "\t")
-	// fmt.Printf(string(jreco))
-
-	emptyPlaylist, playlistCreationErr := client.CreatePlaylistForUser(ctx, user.ID, fmt.Sprintf("spotifyCadence: %d ppm", targetCadence), genericPlaylistDescription, true, false)
-	if playlistCreationErr != nil {
-		log.Fatal("playlist creation error:", playlistCreationErr)
-	}
-
-	recoID := make([]spotify.ID, 0, len(reco.Tracks))
-	for _, v := range reco.Tracks {
-		recoID = append(recoID, v.ID)
-	}
-
-	_, addTracksErr := client.AddTracksToPlaylist(ctx, emptyPlaylist.ID, recoID...)
-	if addTracksErr != nil {
-		log.Fatal("error adding tracks to playlist: ", addTracksErr)
-	}
-
-	fmt.Println("Successfully created playlist ", emptyPlaylist.Name)
+	return b
 }
